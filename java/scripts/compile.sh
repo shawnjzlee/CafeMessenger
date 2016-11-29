7093111ff213889776e07eff9f4413a3af1b0be5
Jeffrey Hsieh <jhsie007@tango-31.cs.ucr.edu>
2016-11-28 18:41:18 -0800

    Update: fix script path.

--- java/scripts/compile.sh
+++ java/scripts/compile.sh
@@ -2,9 +2,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # compile the java program
-javac -d $DIR/../classes $DIR/../src/Cafe.java
+javac -d $DIR/../src $DIR/../src/Cafe.java
 
 #run the java program
 #Use your database name and portss
-java -cp $DIR/../classes:$CLASSPATH Cafe mydb $PGPORT
+java -cp $DIR/../src:$CLASSPATH Cafe mydb $PGPORT
 
--- sql/src/load_data.sql
+++ sql/src/load_data.sql
@@ -1,17 +1,17 @@
 COPY MENU
-FROM 'menu.csv'
+FROM '/home/csmajs/jhsie007/Desktop/CafePOS/data/menu.csv'
 WITH DELIMITER ';';
 
 COPY USERS
-FROM 'users.csv'
+FROM '/home/csmajs/jhsie007/Desktop/CafePOS/data/users.csv'
 WITH DELIMITER ';';
 
 COPY ORDERS
-FROM 'orders.csv'
+FROM '/home/csmajs/jhsie007/Desktop/CafePOS/data/orders.csv'
 WITH DELIMITER ';';
 ALTER SEQUENCE orders_orderid_seq RESTART 87257;
 
 COPY ITEMSTATUS
-FROM 'itemStatus.csv'
+FROM '/home/csmajs/jhsie007/Desktop/CafePOS/data/itemStatus.csv'
 WITH DELIMITER ';';
 
